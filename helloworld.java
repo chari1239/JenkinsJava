a3ea685fdb358aa2e2fa951b238c030c295ca8af
Chari1239 <khva1239@gmail.com>
2019-07-30 15:02:08 +0530

    second commit

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,11 @@
-public void static void main(String[] args) {
+
+public class helloworld {
+
+public static void main(String[] args) {
     
     for(int i=0;i<5;++i)
       System.out.println("Hello World");
 
+ 
+    }
 }
